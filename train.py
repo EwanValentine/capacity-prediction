3b925fbcf2125e3656b5702b44fb345da3d33e2d
ewanvalentine <ewan.valentine@peak.ai>
2019-05-07 21:01:51 +0100

    UPDATED kinda works

--- train.py
+++ train.py
@@ -13,8 +13,8 @@ X = dataset[:, 1] # The train trip
 Y = to_categorical(dataset[:, 0:1]) # The seats taken 1 or 0 for each of 10 seats
 
 model = Sequential()
-model.add(Dense(12, input_shape=(140, 1)))
-model.add(Dense(1))
+model.add(Dense(12, input_shape=(1,)))
+model.add(Dense(88))
 
 model.compile(loss='mean_squared_error',
               optimizer='adam', metrics=['mean_squared_error'])
